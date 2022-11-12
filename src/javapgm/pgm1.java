a0fa4edab20648329c94c12ce68c862304ede7c2
lingaraj12 <117958108+lingaraj12@users.noreply.github.com>
2022-11-11 18:44:23 -0800

    comment to a commit

---
+++ src/javapgm/pgm1.java
@@ -0,0 +1,17 @@
+package javapgm;
+
+public class pgm1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=11;
+	if(a==10)
+	{
+	System.out.println("equal");
+	}
+	else
+	{
+		System.out.println("not equal");
+	}
+}
+}
